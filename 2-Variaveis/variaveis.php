b7738e25bd63e89b568fef50c6eef2c1e404aaed
JoaoSurita <joaopedrosurita490@gmail.com>
2025-08-09 17:05:03 -0300

    Aula sobre Variáveis

---
+++ 2-Variaveis/variaveis.php
@@ -0,0 +1,14 @@
+<?php
+    $nome = (string) "Carlos";
+    $idade = (int) 25;
+    $altura = (float) 1.87;
+    $casado = (bool) false;
+
+
+    echo "$nome <br>";
+    echo "$idade <br>";
+    echo "$altura <br>";
+
+    //echo $nome." tem ".$idade." anos!<br>";
+    //echo "$nome tem $idade anos!";
+?>
