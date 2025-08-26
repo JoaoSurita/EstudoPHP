a34a950a3ee3aff45f06e99e30493bf5a6084417
JoaoSurita <joaopedrosurita490@gmail.com>
2025-08-26 17:13:39 -0300

    Aula sobre visibilidade em um Objeto

---
+++ PHP_POO/Aula03/Caneta.php
@@ -0,0 +1,25 @@
+<?php
+    class Caneta {
+        public $modelo;
+        public $cor;
+        private $ponta;
+        protected $carga;
+        protected $tampada;
+
+        public function rabiscar() {
+            if ($this->tampada == true) {
+                echo "Não Consigo rabiscar pois estou tampada!";
+            } else {
+                echo "Estou rabiscando...";
+            }
+        }
+
+        public function tampar() {
+            $this->tampada = true;
+        }
+
+        public function destampar() {
+            $this->tampada = false;
+        }
+    }
+?>
